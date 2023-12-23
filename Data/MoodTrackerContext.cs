4f1f6ed2d29b7868dfe4cb876fed498a027b9236
ibelichev <59174675+ibelichev@users.noreply.github.com>
2023-12-23 14:31:40 +0300

    Add files via upload

---
+++ Data/MoodTrackerContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoodTracker.Models;
+
+namespace MoodTracker.Data
+{
+    public class MoodTrackerContext : DbContext
+    {
+        public MoodTrackerContext(DbContextOptions<MoodTrackerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DailyMood> DailyMoods { get; set; }
+
+        public DbSet<Mood> Moods { get; set; }
+
+        public DbSet<Event> Events { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DailyMood>().ToTable("DailyMood");
+            modelBuilder.Entity<Mood>().ToTable("Mood");
+            modelBuilder.Entity<Event>().ToTable("Event");
+        }
+    }
+}
+
---
+++ Data/SeedData.cs
@@ -0,0 +1,62 @@
+﻿ using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MoodTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoodTracker.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MoodTrackerContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MoodTrackerContext>>()))
+            {
+                // Look for any DailyMoods.
+                if (context.Moods.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                Mood[] moodSeedData = new Mood[] {
+                    new Mood
+                    {
+                        Id = 1,
+                        Name = "Excited",
+                        Color = "#dd3333"
+                    },
+                    new Mood
+                    {
+                        Id = 2,
+                        Name = "Sad",
+                        Color = "#3333ff"
+                    },
+                    new Mood
+                    {
+                        Id = 3,
+                        Name = "Content",
+                        Color = "#33dd33"
+                    },
+                    new Mood
+                    {
+                        Id = 4,
+                        Name = "Worried",
+                        Color = "#0000ff"
+                    }
+                };
+
+                foreach (Mood m in moodSeedData)
+                {
+                    context.Moods.Add(m);
+                }
+
+                //context.Moods.AddRange();
+                context.SaveChanges();
+            }
+        }
+    }
+}
